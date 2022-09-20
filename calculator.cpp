8941f169143665c52d29adc1a3e93a0abf41a91c
Valera <78586195+ValeriiBabich@users.noreply.github.com>
2022-09-20 22:11:15 +0200

    finish

--- calculator.cpp
+++ calculator.cpp
@@ -9,3 +9,13 @@ int Calculator::Sub (double a, double b)
 {
 	return Add (a, -b);
 }
+
+int Calculator::Min (double a, double b)
+{
+	return a - b;
+}
+
+int Calculator::Del (double a, double b)
+{
+	return a / b;
+}
